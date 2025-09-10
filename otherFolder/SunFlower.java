5ae411f5c14f0d8c456519ab220756e406470c44
marcoyimyaem <marcoyimyaem@gmail.com>
2025-09-10 03:44:01 +0000

    Day 22

--- otherFolder/SunFlower.java
+++ otherFolder/SunFlower.java
@@ -1,9 +1,23 @@
 package otherFolder;
 import chapter1.*;
+import chapter4.AccessModifier;
 
 public class SunFlower {
     public static void main(String[] args) {
         Toy toy1 = new Toy();
         System.out.println(toy1);
-         }
+        AccessModifier acm1 = new AccessModifier();
+        acm1.greetMe();
+        // acm1.greetMe2();
+        // acm1.greetMe3();
+        // acm1.greetMe4();
+    }
+}
+class MoonFlower extends AccessModifier{
+    public static void main(String[] args) {
+        MoonFlower mf = new MoonFlower();
+        mf.greetMe();
+        mf.greetMe3();
+        // mf.greetMe4();
+        }
 }
